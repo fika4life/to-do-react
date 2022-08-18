129ecadc2c4ca555d21b5fe53a8da24374173fd7
fika4life <fistewie@gmail.com>
2022-08-18 21:35:02 +0200

    adds some styling to list and items

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {
       fontFamily: {
-        josefin: ['"Josefin Sans"', "sans"],
+        josefin: ['"Josefin Sans"', 'sans']
       },
+
       colors: {
-        brightBlue: "hsl(220, 98%, 61%)",
-        checkBackground:
-          "linear-gradient hsl(192, 100%, 67%) to hsl(280, 87%, 65%)",
-        veryLightGray: "hsl(0, 0%, 98%)",
-        veryLightGrayishBlue: "hsl(236, 33%, 92%)",
-        lightGrayishBlue: "hsl(233, 11%, 84%)",
-        darkGrayishBlue: "hsl(236, 9%, 61%)",
-        veryDarkGrayishBlue: "hsl(235, 19%, 35%)",
-      },
-    },
+        brightBlue: 'hsl(220, 98%, 61%)',
+        veryLightGray: 'hsl(0, 0%, 98%)',
+        veryLightGrayishBlue: 'hsl(236, 33%, 92%)',
+        lightGrayishBlue: 'hsl(233, 11%, 84%)',
+        darkGrayishBlue: 'hsl(236, 9%, 61%)',
+        veryDarkGrayishBlue: 'hsl(235, 19%, 35%)'
+      }
+    }
   },
-  plugins: [],
+  plugins: [require('@tailwindcss/forms')]
 };
