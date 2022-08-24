70d68b05bfd9fb1efe124a7063861e1b38a9a544
Fiona Karlsson <fionak@spotify.com>
2022-08-24 14:14:05 +0200

    adds checkmark

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
       backgroundImage: {
-        'hero-image': "url('./assets/images/bg-desktop-light.jpg')"
+        "hero-image": "url('./assets/images/bg-desktop-light.jpg')",
       },
       fontFamily: {
-        josefin: ['"Josefin Sans"', 'sans']
+        josefin: ['"Josefin Sans"', "sans"],
       },
 
       colors: {
-        brightBlue: 'hsl(220, 98%, 61%)',
-        veryLightGray: 'hsl(0, 0%, 98%)',
-        veryLightGrayishBlue: 'hsl(236, 33%, 92%)',
-        lightGrayishBlue: 'hsl(233, 11%, 84%)',
-        darkGrayishBlue: 'hsl(236, 9%, 61%)',
-        veryDarkGrayishBlue: 'hsl(235, 19%, 35%)'
-      }
-    }
+        brightBlue: "hsl(220, 98%, 61%)",
+        veryLightGray: "hsl(0, 0%, 98%)",
+        veryLightGrayishBlue: "hsl(236, 33%, 92%)",
+        lightGrayishBlue: "hsl(233, 11%, 84%)",
+        darkGrayishBlue: "hsl(236, 9%, 61%)",
+        veryDarkGrayishBlue: "hsl(235, 19%, 35%)",
+      },
+    },
   },
-  plugins: [require('@tailwindcss/forms')]
+  plugins: [require("@tailwindcss/forms")],
 };
