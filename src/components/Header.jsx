0d9f6391b6f9297de3bbefe7926834b0b14a5e39
Fiona Karlsson <fionak@spotify.com>
2022-08-25 09:30:08 +0200

    adds click event to dark mode icon

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,21 @@
-import MoonIcon from '../assets/images/icon-moon.svg';
+import MoonIcon from "../assets/images/icon-moon.svg";
 
 function Header() {
+  const handleClick = (e) => {
+    let mode = e.target.classList[0];
+    console.log(mode);
+  };
   return (
     <div className="flex justify-between ">
-      <h1 className="uppercase text-5xl tracking-widest mb-8 text-white">
+      <h1 className="uppercase text-5xl tracking-widest mb-8  text-white">
         Todo
       </h1>
-      <img src={MoonIcon} alt="" className="w-[30px] h-[30px] " />
+      <img
+        src={MoonIcon}
+        alt=""
+        className="light w-[30px] h-[30px] cursor-pointer "
+        onClick={handleClick}
+      />
     </div>
   );
 }
